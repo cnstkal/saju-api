3020dbf824ca8d488cade87bc8d84ce8e673354a
cnstkal <anihan1230@gmail.com>
2026-07-26 23:13:14 +0900

    add cors

--- dist/index.js
+++ dist/index.js
@@ -4,8 +4,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
 const saju_1 = __importDefault(require("./src/routes/saju"));
 const app = (0, express_1.default)();
+app.use((0, cors_1.default)());
 app.use(express_1.default.json());
 app.use("/saju", saju_1.default);
 app.get("/", (req, res) => {
